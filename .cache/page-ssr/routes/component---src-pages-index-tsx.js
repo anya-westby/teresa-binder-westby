20232271b62b506b16197d1d61d6fa168ed32583
Anya Westby <anyawestby@gmail.com>
2024-12-02 21:06:05 -0500

    feat: second batch of dependencies

---
+++ .cache/page-ssr/routes/component---src-pages-index-tsx.js
@@ -0,0 +1,16 @@
+exports.id = "component---src-pages-index-tsx";
+exports.ids = ["component---src-pages-index-tsx"];
+exports.modules = {
+
+/***/ "./src/pages/index.tsx?export=default":
+/*!********************************************!*\
+  !*** ./src/pages/index.tsx?export=default ***!
+  \********************************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+};
+;

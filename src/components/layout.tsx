db542afe9c7cdd6b0eec0c0df06111dc53df4e4d
Anya Westby <anyawestby@gmail.com>
2025-01-23 17:39:17 -0500

    feat: update styles

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,22 @@
 import * as React from "react";
 import { GlobalStyle } from "../styledComponents/globals";
+import { useEffect, useState } from "react";
 
 const Layout = ({ children }) => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   return (
     <>
       <GlobalStyle />
-      <main>{children}</main>
+      <main
+        style={{ opacity: mounted ? 1 : 0, transition: "opacity 0.3s ease" }}
+      >
+        {children}
+      </main>
     </>
   );
 };
